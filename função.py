389bad4f1c153c392130ac25548f7df8ec777f25
AlencarSQL <yuriclayalencar@gmail.com>
2025-10-01 10:56:56 -0300

    Create função.py

---
+++ função.py
@@ -0,0 +1,15 @@
+def media(notas):
+
+    if len(notas) == 0:
+        return 0
+    return sum(notas) / len(notas)
+
+
+def maior(notas):
+
+    return max(notas)
+
+
+def menor(notas):
+
+    return min(notas)
